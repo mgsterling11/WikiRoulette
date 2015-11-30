f88887c7e395756c1f4be2b468af6399789decc4
mgsterling11 <m.g.sterling11@gmail.com>
2015-11-30 17:03:30 -0500

    Completes Jquery/AJAX functionality:
    Refactors javascript, now functional;
    
    cleans up views

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,9 @@
 $(function(){
-  $('a').on('ajax:success', function(e, data, status, xhr) {
+    $(document).on('ajax:success', 'a', function(e, data, status, xhr){
     $('.wikis').empty();
-    $('.wikis').prepend(xhr.responseText);
+    $('.wikis').prepend(xhr.responseText.slice(2055, -251));
 
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     $('body').css('background-color', '#' + randomColor)
-
+    })
   })
-})
