25cdcba10b4fa5bdc39df3815dbef1fd8abb5719
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-11 16:38:19 -0500

    fixes product view bug

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,7 +2,7 @@ $(function(){
     $(document).on('ajax:success', 'a', function(e, data, status, xhr){
     $('.instructions').empty();
     $('.wikis').empty();
-    $('.wikis').prepend(xhr.responseText.slice(5600,-40));
+    $('.wikis').prepend(xhr.responseText.slice(1975,-40));
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     $('body').css('background-color', '#' + randomColor)
     })
