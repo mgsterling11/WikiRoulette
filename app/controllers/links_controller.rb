819901bc0d326db1954bd4cd87331ee8b86bedcd
Michael Sterling <michael.sterling@venuenext.com>
2016-01-28 11:30:27 -0500

    test commit

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,15 +1,15 @@
 class LinksController < ApplicationController
-    
+
   def index
     @links = Scraper.new.generate_links(params['name'])
     render "welcome/welcome_page", locals: {:links => @links}
   end
 
   def wiki_pages
+    # binding.pry
     @wikis = WikipediaPage.new.generate_wiki_links_path
     Scraper.clear
     render "welcome/welcome_page", locals: {:wikis => @wikis}
   end
-    
-end
 
+end
